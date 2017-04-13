ef1cb25416a12ec3c839393aa88d868f68683b12
Prakash <sthaprakash91@gmail.com>
2017-04-13 12:34:47 +0545

    detail page image display done

--- resources/views/shop/detail.blade.php
+++ resources/views/shop/detail.blade.php
@@ -29,20 +29,27 @@
                         <div class="col-sm-4 col-sm-offset-1 col-xs-6 zoom-left">
 
                             @if(isset($product_image))
-                                @foreach($product_image as $img)
-                            <div id="zoom-click" class="owl-carousel owl-theme">
-                                <div class="item" data-hash="zero{{$img['id']}}">
-                                    <div class="product-main-image" style="position: relative; overflow: hidden;">
-                                        <img  src="{{asset('storage/image/product')}}/{{$img['image']}}" alt="Cool green dress with red bell" class="img-responsive">
-                                        <img  src="{{asset('storage/image/product')}}/{{$img['image']}}"  class="zoomImg" style="position: absolute; top: -247.909px; left: -269.182px; opacity: 0; width: 600px; height: 800px; border: medium none;">
-                                    </div>
+                                <div id="zoom-click" class="owl-carousel owl-theme">
+                                    @foreach($product_image as $img)
+                                        <div class="item" data-hash="zero{{$img['id']}}">
+                                            <div class="product-main-image"
+                                                 style="position: relative; overflow: hidden;">
+                                                <img src="{{asset('storage/image/product')}}/{{$img['image']}}"
+                                                     alt="Cool green dress with red bell" class="img-responsive">
+                                                <img src="{{asset('storage/image/product')}}/{{$img['image']}}"
+                                                     class="zoomImg"
+                                                     style="position: absolute; top: -247.909px; left: -269.182px; opacity: 0; width: 600px; height: 800px; border: medium none;">
+                                            </div>
+                                        </div>
+                                    @endforeach
                                 </div>
-                            </div>
-                            <a class="button secondary1" href="#zero{{$img['id']}}">
-                                <img src="{{asset('storage/image/product')}}/{{$img['image']}}"> </a>
-                            @endforeach
+                                @foreach($product_image as $img)
+                                    <a class="button secondary1" href="#zero{{$img['id']}}">
+                                        <img src="{{asset('storage/image/product')}}/{{$img['image']}}"> </a>
+                                @endforeach
                             @endif
                         </div>
+
                         <div class="col-sm-6 col-sm-offset-1 col-xs-6 zoom-right">
                             <h2>{{strtoupper($productid->name)}}</h2>
                             <div class="price-availability-block clearfix">
@@ -54,9 +61,9 @@
                                         @endforeach
                                     @endif
                                     @if(isset($product_desc))
-                                    <p> &emsp;&emsp;* &ensp;{{$product_desc->description}}</p>
-                                    <p> &emsp;&emsp;* &ensp;{{$product_desc->information}}</p>
-                                        @endif
+                                        <p> &emsp;&emsp;* &ensp;{{$product_desc->description}}</p>
+                                        <p> &emsp;&emsp;* &ensp;{{$product_desc->information}}</p>
+                                    @endif
                                     <h3> Buy rudrax with </h3>
 
                                 </div>
@@ -64,7 +71,8 @@
                             @foreach($capping as $cap)
                                 <div class="funkyradio">
                                     <div class="funkyradio-default ">
-                                        <input type="radio" name="capping" class="oddtick-box"  id="radio{{$cap->id}}" value="{{$cap->price}}"/>
+                                        <input type="radio" name="capping" class="oddtick-box" id="radio{{$cap->id}}"
+                                               value="{{$cap->price}}"/>
                                         <label for="radio{{$cap->id}}">{{$cap->type}} Capping</label>
                                     </div>
                                 </div>
@@ -74,10 +82,10 @@
                             </div>
 
                             <div class="sticker sticker-sale"></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     </section>
 
@@ -95,9 +103,9 @@
                             <div id="home" class="tab-pane fade in active">
                                 <h3 class="tab-header"> Description </h3>
                                 @if(isset($product_desc))
-                                <p class="tab-detail">&emsp;{{$product_desc->description}}<br>
-                                    &emsp;{{$product_desc->information}}</p>
-                                    @endif
+                                    <p class="tab-detail">&emsp;{{$product_desc->description}}<br>
+                                        &emsp;{{$product_desc->information}}</p>
+                                @endif
                             </div>
                             <div id="menu1" class="tab-pane fade">
                                 @if(isset($benefits))
